e7031c66a1fe27185bb7969fccbc6d436bd6b269
Dang Nguyen Tien Hai <nthdangg@gmail.com>
2025-12-04 16:05:15 +0000

    [DangNTH] add utils layer

---
+++ Ultils/Utils.h
@@ -0,0 +1,17 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+/**
+ * @brief  Deletes a book from the list.
+ *
+ * Returns BOOK_SUCCESS_DELETE on success,
+ * BOOK_DELETE_FAILED_NOT_FOUND if the book is missing,
+ * or BOOK_DELETE_FAILED_BORROWED if the book is currently borrowed.
+ *
+ * @param books   Array of all books.
+ * @param n       Pointer to the number of books.
+ *
+ * @return BookRetCode_t  Status code of the delete operation.
+ */
+
+#endif /* _UTILS_H_ */
